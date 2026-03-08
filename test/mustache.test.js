b76c8cf184e4fc20eccc2ad3728526bc1f753605
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-08 19:38:48 +0700

    mustache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -4,3 +4,9 @@ test("Menggunakan mustache", () => {
   const data = Mustache.render(`Hello {{ name }}`, { name: "Elham" });
   expect(data).toBe("Hello Elham");
 });
+
+test("Melakukan Compile", () => {
+  Mustache.parse("Hello, {{ name }}");
+  const data = Mustache.render("Hello, {{ name }}", { name: "Elham" });
+  expect(data).toBe("Hello, Elham");
+});
