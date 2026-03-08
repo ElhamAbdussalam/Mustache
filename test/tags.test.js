b51c5ac7a449d60a258b8191697ee76ced7906a4
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-08 19:46:59 +0700

    tags

--- test/tags.test.js
+++ test/tags.test.js
@@ -7,3 +7,12 @@ test("Tags", () => {
   });
   expect(data).toBe("Hello, Elham, my hobby is <b>Programming</b>");
 });
+
+test("Nested Object", () => {
+  const data = Mustache.render("Hello, {{ person.name }}", {
+    person: {
+      name: "Elham",
+    },
+  });
+  expect(data).toBe("Hello, Elham");
+});
