c2b5040df74939d4a6c2c704f9117a58758bac96
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-09 05:12:04 +0700

    section show

--- test/section-show.test.js
+++ test/section-show.test.js
@@ -11,3 +11,18 @@ test("Section Not Show", async () => {
   console.info(data);
   expect(data).not.toContain("Hello Person");
 });
+
+test("Sections Show", async () => {
+  const helloTemplate = await fs
+    .readFile("./template/hello.mustache")
+    .then((data) => data.toString());
+
+  const data = Mustache.render(helloTemplate, {
+    person: {
+      name: "Elham",
+    },
+  });
+
+  console.info(data);
+  expect(data).toContain("Hello Person");
+});
