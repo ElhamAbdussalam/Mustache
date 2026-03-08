965b446068fd207b87d219c381abed24602c2161
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-09 05:09:16 +0700

    section not show

---
+++ test/section-not-show.test.js
@@ -0,0 +1,13 @@
+import Mustache from "mustache";
+import fs from "fs/promises";
+
+test("Section Not Show", async () => {
+  const helloTemplate = await fs
+    .readFile("./template/hello.mustache")
+    .then((data) => data.toString());
+
+  const data = Mustache.render(helloTemplate, {});
+
+  console.info(data);
+  expect(data).not.toContain("Hello Person");
+});
